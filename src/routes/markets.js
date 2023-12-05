367c3d24b8e3c2b5408c5c48c87572573ef7c569
Yumin Kim <49779139+Yumin2019@users.noreply.github.com>
2023-12-05 23:21:15 +0900

    feat: queryParam 처리 추가

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -4,18 +4,41 @@ const router = Router();
 
 const supermarkets = [
   {
+    id: 1,
     store: "whoe foods",
+    miles: 0.6,
   },
   {
+    id: 2,
     store: "poonam store",
+    miles: 2.5,
   },
   {
+    id: 3,
     store: "yumin store",
+    miles: 2.8,
+  },
+  {
+    id: 4,
+    store: "sinsung store",
+    miles: 4,
+  },
+  {
+    id: 5,
+    store: "real store",
+    miles: 5,
   },
 ];
 
 router.get("/", (req, res) => {
-  res.send(supermarkets);
+  console.log(req.query);
+  const { miles } = req.query;
+  const parsedMiles = parseInt(miles);
+  if (miles && !isNaN(parsedMiles)) {
+    res.send(supermarkets.filter((s) => s.miles <= miles));
+  } else {
+    res.send(supermarkets);
+  }
 });
 
 module.exports = router;
